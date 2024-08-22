e3e6dd9102344c3effed83e334ac469a0cf779f3
Sakhavat1996 <sakhavat1996@gmail.com>
2024-08-22 10:10:39 +0400

    error-handling & not Found

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return(
+        <main className="not-found">
+            <h1>Tapilmadi</h1>
+            <p>Teessuf ki, bu sorgu atdiginiz yerden netice yoxdur</p>
+        </main>
+    )
+}
